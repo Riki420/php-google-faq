70d5414cca8b6aded92e83710998f943a33e058e
Riki420 <girottiriccardo1998@gmail.com>
2021-09-22 15:44:20 +0200

    aggiunte FAQS

--- index.php
+++ index.php
@@ -10,6 +10,34 @@
                             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
 
         ],
+        [
+            'title' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+            'paragraph' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+                            Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
+                            Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
+                            Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
+
+        ],
+        [
+            'title' => "Perché il mio account è associato a un paese?",
+            'paragraph' => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+                            <ol>
+                                <li>
+                                    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                                    <ol>
+                                        <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                                        <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+                                    </ol>
+                                </li>
+                                <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+                            </ol>
+                            Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
+                            <h4>Stabilire il paese associato al tuo account</h4>
+                            Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
+                            I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+                            Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato."           
+        ],
+        
     ]
 
 ?>
@@ -23,6 +51,12 @@
     <title>PHP Google FAQ</title>
 </head>
 <body>
-    
+    <!--Ciclo sull'array FAQS per ogni faq dentro di esso-->
+    <?php for ($i = 0; $i < count($faqs); $i++) { ?>
+        <!--Stampo il risultato tramite la chiave--->
+        <h1><? echo $faqs[$i]['title'] ?></h1>
+        <p><? echo $faqs[$i]['paragraph'] ?></p>
+
+    <?php } ?>
 </body>
 </html>
